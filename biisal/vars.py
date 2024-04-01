39be5dcc78d5101054b88308a8d5a02432f4af52
itsDev-Developer <148090753+itsDev-Developer@users.noreply.github.com>
2024-04-02 00:45:35 +0530

    Update vars.py
    Dine

--- biisal/vars.py
+++ biisal/vars.py
@@ -32,7 +32,7 @@ class Var(object):
         APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
     
     else:
-        ON_HEROKU = True
+        ON_HEROKU = False
     FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',True))
     if HAS_SSL:
